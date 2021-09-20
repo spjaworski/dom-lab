7a95a75852c17ae8548c1bd2f15f1978b8087596
Scott Jaworski <scottpjaworski@gmail.com>
2021-09-20 17:56:27 -0400

    Squares now change color

--- scripts.js
+++ scripts.js
@@ -13,11 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     squareNumber = 0;
 
+    var colors = ["magenta", "blue", "red", "grey", "green", "brown", "purple", "yellow"];
+
+    function randomColor() {
+        return colors[Math.floor(Math.random() * colors.length)]
+    };
+
     function insertSquare() {
         let newSquare = document.createElement('div');
         newSquare.className = 'block';
         newSquare.id = squareNumber;
         blockRow.appendChild(newSquare);
+        newSquare.addEventListener('click', function(){
+            newSquare.style.backgroundColor = randomColor();
+        })
     }
 
     squareButton.addEventListener('click', function(){
@@ -25,4 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(squareNumber);
         squareNumber += 1;
       })
+
+
 });
