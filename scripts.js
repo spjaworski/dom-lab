6f27c8f9a65b196a7edff17c612aba99cb22b005
Scott Jaworski <scottpjaworski@gmail.com>
2021-09-20 14:12:15 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let squareButton = document.createElement('button');
+    let squareButtonText= document.createTextNode('Add Square');
+    squareButton.appendChild(squareButtonText);
+    document.body.appendChild(squareButton);
+
+    function insertSquare() {
+        let newSquare = document.createElement('div');
+        newSquare.className = 'block';
+        document.body.appendChild(newSquare);
+    }
+
+    squareButton.addEventListener('click', function(){
+        insertSquare();
+    })
+});
